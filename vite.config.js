d1084529857d2583ff33a971ce41156b69def53f
Wemdio <vaver1954@mail.ru>
2025-12-01 15:06:53 +0400

    chore(build): enable terser minification and console removal

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,13 @@ export default defineConfig({
     host: true
   },
   build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
     rollupOptions: {
       output: {
         manualChunks: {
